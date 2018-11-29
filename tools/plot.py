396f3c1ef5a2fec352c1637384e534af1a16e2d3
CHEN Shen <675121663@qq.com>
2018-11-29 19:47:57 +0800

    增加一个绘图辅助函数

--- tools/plot.py
+++ tools/plot.py
@@ -38,3 +38,31 @@ def plot_iter_curve(scores, title=''):
     plt.xlabel('iter t')
     plt.ylabel('score d')
     plt.show()
+
+
+def draw_H_and_E(citys, H_path, energys):
+    """
+    可视化画出哈密顿回路和能量趋势
+    :param citys:
+    :param H_path:
+    :param energys:
+    :return:
+    """
+    fig = plt.figure()
+    # 绘制哈密顿回路
+    ax1 = fig.add_subplot(121)
+    plt.xlim(0, 7)
+    plt.ylim(0, 7)
+    for (from_, to_) in H_path:
+        p1 = plt.Circle(citys[from_], 0.2, color='red')
+        p2 = plt.Circle(citys[to_], 0.2, color='red')
+        ax1.add_patch(p1)
+        ax1.add_patch(p2)
+        ax1.plot((citys[from_][0], citys[to_][0]), (citys[from_][1], citys[to_][1]), color='red')
+        # ax1.annotate(to_, xy=citys[to_], xytext=(-8, -4), textcoords='offset points', fontsize=8)
+    ax1.axis('equal')
+    ax1.grid()
+    # 绘制能量趋势图
+    ax2 = fig.add_subplot(122)
+    ax2.plot(np.arange(0, len(energys), 10), energys[::10], color='red')
+    plt.show()
